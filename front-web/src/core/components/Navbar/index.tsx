39ef5fd8c27b4c8ed3b78c317e862399a4ac244b
Vitor Lucas <vitorluc.2013@gmail.com>
2021-02-08 11:32:41 -0300

    Add Navbar

---
+++ front-web/src/core/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Navbar = () => (
+    <nav className="main-nav">
+            <a href="home" className='nav-title'>
+                MovieFlix
+            </a>
+        </nav>
+);
+
+export default Navbar;
