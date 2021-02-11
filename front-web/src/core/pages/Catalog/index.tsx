2acdae2f0ccfc5ce5393dad5cbe5c26bb61bcfa9
Vitor Lucas <vitorluc.2013@gmail.com>
2021-02-11 19:10:15 -0300

    Add LoginCard

--- front-web/src/core/pages/Catalog/index.tsx
+++ front-web/src/core/pages/Catalog/index.tsx
@@ -1,9 +1,23 @@
+import MovieCard from './components/MovieCard';
 import './styles.css';
-
-const Catalog = () =>(
+const Catalog = () => (
     <div className='catalog-container'>
-        <div className='catalog-search'>
-
+        <div className='catalog-content'>
+            <div className="catalog-filter">
+                <h1>Aventura</h1>
+            </div>
+            <div className="catalog-movies">
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+            </div>
         </div>
     </div>
 );
